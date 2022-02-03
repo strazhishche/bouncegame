4f6c41bcc28bfaca3a83b50c5edac77b94ba48d8
strazhishche <anderreflecs@gmail.com>
2022-02-03 21:33:49 +0300

    format

--- gamebounce.py
+++ gamebounce.py
@@ -16,14 +16,17 @@ class Ball:
         self.y = 0
         self.canvas.bind_all('<KeyPress-Return>', self.start)
         self.bounce_count = 0
+
     def bounce(self):
         self.bounce_count += 1
+
     def hit_paddle(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
         if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
             if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
                 return True
             return False
+
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
@@ -39,6 +42,7 @@ class Ball:
             self.x *= -1
         if pos[2] >= self.canvas_width:
             self.x *= -1
+
     def start(self, evt):
         if self.started == False:
             starts = [-3, -2, -1, 1, 2, 3]
@@ -46,8 +50,6 @@ class Ball:
             self.x = starts[0]
             self.y = -3
             self.started = True
-
-
 
 class Paddle:
     def __init__(self, canvas, color):
@@ -58,6 +60,7 @@ class Paddle:
         self.canvas_width = self.canvas.winfo_width()
         self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
         pos = self.canvas.coords(self.id)
@@ -65,14 +68,17 @@ class Paddle:
             self.x = 0
         elif pos[2] >= self.canvas_width:
             self.x = 0
+
     def turn_left(self, evt):
         pos = self.canvas.coords(self.id)
         if pos[0] > 0:
             self.x = -2
+
     def turn_right(self, evt):
         pos = self.canvas.coords(self.id)
         if pos[2] < self.canvas_width:
             self.x = 2
+
     def destroy(self):
         self.destroy()
 
